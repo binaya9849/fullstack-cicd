9e581b807f3dad3223aad0ac375dca8a9a21c61a
binaya9849 <xandermaharjan@gmail.com>
2026-05-10 01:44:25 +0300

    automation

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express()
 
 const PORT = process.env.PORT || 5001
 
+// Home route
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// 👉 ADD THIS HEALTH CHECK ROUTE
+app.get('/health', (req, res) => {
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
