ea82eb194351c2f5ce9875da33ebf0db155a6874
binaya9849 <xandermaharjan@gmail.com>
2026-05-10 03:27:23 +0300

    chang

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5001
 
-
+// Serve React build
 app.use(express.static(path.join(__dirname, 'dist')))
 
-
+// API routes
 app.get('/health', (req, res) => {
   res.send('ok')
 })
@@ -16,11 +16,12 @@ app.get('/test', (req, res) => {
   res.send('hello from feature branch')
 })
 
-
+// React fallback route (important)
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
